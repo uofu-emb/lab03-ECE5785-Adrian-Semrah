055ff34a9526a9b6396d6212a3ae7728b41ca36c
JTAG <jtag@debian12.housing.utah.edu>
2024-09-23 13:10:27 -0600

    Setup Unit Tests and Compiled

--- include/threads_functions.h
+++ include/threads_functions.h
@@ -1,4 +1,14 @@
 
+#ifndef _THREAD_FUNCTIONS_H_
+#define _THREAD_FUNCTIONS_H_
+
+#include <stdio.h>
+#include <FreeRTOS.h>
+#include <semphr.h>
+#include <task.h>
+
 void big_increment_print(SemaphoreHandle_t semaphore, int counter);
 
 void increment_print(SemaphoreHandle_t semaphore, int counter);
+
+#endif
--- test/test.c
+++ test/test.c
@@ -4,25 +4,61 @@
 #include <unity.h>
 #include "unity_config.h"
 
+#include <FreeRTOS.h>
+#include <semphr.h>
+#include <task.h>
 
+#include "threads_functions.h"
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_variable_assignment()
+void test_big_increment_print()
 {
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+    int counter = 0;
+
+    SemaphoreHandle_t semaphore = xSemaphoreCreateCounting(1, 1);
+
+    big_increment_print(semaphore, counter);
+    TEST_ASSERT_TRUE_MESSAGE(counter == 1, "Big increment print failed at 0");
+
+    big_increment_print(semaphore, counter);
+    TEST_ASSERT_TRUE_MESSAGE(counter == 3, "Big increment print failed at 1");
+
+    big_increment_print(semaphore, counter);
+    TEST_ASSERT_TRUE_MESSAGE(counter == 7, "Big increment print failed at 2");
 }
 
-v
+void test_increment_print()
+{
+    int counter = 0;
+
+    SemaphoreHandle_t semaphore = xSemaphoreCreateCounting(1, 1);
+
+    increment_print(semaphore, counter);
+    TEST_ASSERT_TRUE_MESSAGE(counter == 1, "Increment print failed at 0");
+
+    increment_print(semaphore, counter);
+    TEST_ASSERT_TRUE_MESSAGE(counter == 2, "Increment print failed at 1");
+
+    increment_print(semaphore, counter);
+    TEST_ASSERT_TRUE_MESSAGE(counter == 3, "Increment print failed at 2");
+}
+
+
 
 int main (void)
 {
     stdio_init_all();
+    sleep_ms(5000);
+    printf("Starting Tests\n");
+
     UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
+    RUN_TEST(test_big_increment_print);
+    RUN_TEST(test_increment_print);
+
+    sleep_ms(5000);
+
     return UNITY_END();
 }
